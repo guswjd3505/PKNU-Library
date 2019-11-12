7a12ea2b3f0dcaa867ff35dc5d4eae8fd3a03ec1
tk6790 <tk6790@hanmail.net>
2019-11-13 00:06:47 +0900

    5주차 PPT / BeaActivity, ResActivity Layout

--- PKNULibrary/app/src/main/java/com/example/pknulibrary/BeaActivity.java
+++ PKNULibrary/app/src/main/java/com/example/pknulibrary/BeaActivity.java
@@ -2,13 +2,31 @@ package com.example.pknulibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class BeaActivity extends AppCompatActivity {
+public class BeaActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bea);
+
+        //버튼
+        Button button = findViewById(R.id.button);
+
+        //버튼실행
+        button.setOnClickListener(new OnClickListener()) {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(BeaActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 }
