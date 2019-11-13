6d389023fc1ea20aa72e0a4ff2d8924d354ba6e2
Hyun-Jeong Kim <kimhyen82@gmail.com>
2019-11-14 00:34:28 +0900

    add/ Mypage.java, oneA.java, mypage.xml 수정

--- PKNULibrary/app/src/main/java/com/example/pknulibrary/MyPageActivity.java
+++ PKNULibrary/app/src/main/java/com/example/pknulibrary/MyPageActivity.java
@@ -2,14 +2,26 @@ package com.example.pknulibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class MyPageActivity extends AppCompatActivity {
 
     TextView textView;
+    Button extend, seat, empty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +29,76 @@ public class MyPageActivity extends AppCompatActivity {
 
         textView=findViewById(R.id.date);
 
-        Intent intent=getIntent();
-        String name=intent.getStringExtra("name");
-        textView.setText(name);
+        extend = findViewById(R.id.extend);
+        seat = findViewById(R.id.seat);
+        empty = findViewById(R.id.empty);
+
+
+
+        extend.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                TimeZone timezone = TimeZone.getTimeZone("Etc/GMT-9");
+                TimeZone.setDefault(timezone);
+
+                SimpleDateFormat formater = new SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.KOREA);
+
+                Date current = new Date();
+
+                String mtime = formater.format(current);
+                System.out.println(mtime);
+
+                TimeZone jst = TimeZone.getTimeZone ("JST");
+                Calendar cal = Calendar.getInstance(Locale.KOREA); // 주어진 시간대에 맞게 현재 시각으로 초기화된 GregorianCalender 객체를 반환.// 또는 Calendar now = Calendar.getInstance(Locale.KOREA);
+                System.out.println ( cal.get ( Calendar.YEAR ) + "." + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ "." +cal.get ( Calendar.MINUTE ));
+                textView.setText("번 좌석 사용 중"+"\n\n"+mtime+" ~ "+cal.get ( Calendar.YEAR ) + " . " + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ ":" +cal.get ( Calendar.MINUTE ));
+
+                //startActivity(intent);
+
+                return true;
+            }
+
+        });
+
+        seat.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                textView.setText(" 현재 좌석\n\n 남은 시간");
+                return true;
+            }
+        });
+
+        empty.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        saveState();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        restoreSatate();
+    }
+
+    protected void saveState(){
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name",textView.getText().toString());
+        editor.commit();
+    }
+
+    protected void restoreSatate(){
+        SharedPreferences pref = getSharedPreferences("pref",Activity.MODE_PRIVATE);
+        if((pref!=null)&&(pref.contains("name"))){
+            String name = pref.getString("name","남은시간");
+            textView.setText(name);
+        }
     }
 
 }
--- PKNULibrary/app/src/main/java/com/example/pknulibrary/oneA.java
+++ PKNULibrary/app/src/main/java/com/example/pknulibrary/oneA.java
@@ -2,13 +2,16 @@ package com.example.pknulibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,7 +23,7 @@ public class oneA extends AppCompatActivity {
 
     TextView display;
 
-    Button btn1;
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +34,14 @@ public class oneA extends AppCompatActivity {
 
 
         btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        final String btn1text = btn1.getText().toString();
+        final String btn2text = btn2.getText().toString();
 
         btn1.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View arg0, MotionEvent arg1) {
+
+
                 TimeZone timezone = TimeZone.getTimeZone("Etc/GMT-9");
                 TimeZone.setDefault(timezone);
 
@@ -47,19 +55,71 @@ public class oneA extends AppCompatActivity {
                 TimeZone jst = TimeZone.getTimeZone ("JST");
                 Calendar cal = Calendar.getInstance(Locale.KOREA); // 주어진 시간대에 맞게 현재 시각으로 초기화된 GregorianCalender 객체를 반환.// 또는 Calendar now = Calendar.getInstance(Locale.KOREA);
                 System.out.println ( cal.get ( Calendar.YEAR ) + "." + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ "." +cal.get ( Calendar.MINUTE ));
-                display.setText(mtime+" ~ "+cal.get ( Calendar.YEAR ) + " . " + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ ":" +cal.get ( Calendar.MINUTE ));
+                display.setText(btn1text+"번 좌석 사용 중"+"\n\n"+mtime+" ~ "+cal.get ( Calendar.YEAR ) + " . " + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ ":" +cal.get ( Calendar.MINUTE ));
 
                 Intent intent=new Intent(oneA.this,MyPageActivity.class);
                 intent.putExtra("name",display.getText().toString());
-                startActivity(intent);
+                //startActivity(intent);
 
                 return true;
             }
         });
 
+        btn2.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
 
 
+                TimeZone timezone = TimeZone.getTimeZone("Etc/GMT-9");
+                TimeZone.setDefault(timezone);
 
+                SimpleDateFormat formater = new SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.KOREA);
 
+                Date current = new Date();
+
+                String mtime = formater.format(current);
+                System.out.println(mtime);
+
+                TimeZone jst = TimeZone.getTimeZone ("JST");
+                Calendar cal = Calendar.getInstance(Locale.KOREA); // 주어진 시간대에 맞게 현재 시각으로 초기화된 GregorianCalender 객체를 반환.// 또는 Calendar now = Calendar.getInstance(Locale.KOREA);
+                System.out.println ( cal.get ( Calendar.YEAR ) + "." + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ "." +cal.get ( Calendar.MINUTE ));
+                display.setText(btn2text+"번 좌석 사용 중"+"\n\n"+mtime+" ~ "+cal.get ( Calendar.YEAR ) + " . " + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ ":" +cal.get ( Calendar.MINUTE ));
+
+                Intent intent=new Intent(oneA.this,MyPageActivity.class);
+                intent.putExtra("name",display.getText().toString());
+
+                Intent intent1 = new Intent(oneA.this,MyPageActivity.class);
+                intent1.putExtra("name",btn2text);
+
+                return true;
+            }
+        });
     }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        saveState();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        restoreSatate();
+    }
+
+    protected void saveState(){
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name",display.getText().toString());
+        editor.commit();
+    }
+
+    protected void restoreSatate(){
+        SharedPreferences pref = getSharedPreferences("pref",Activity.MODE_PRIVATE);
+        if((pref!=null)&&(pref.contains("name"))){
+            String name = pref.getString("name","");
+            display.setText(name);
+        }
+    }
+
 }
