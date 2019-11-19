e496cf8e2a77781f377f036049c978aa27af8054
Hyun-Jeong Kim <kimhyen82@gmail.com>
2019-11-19 23:30:05 +0900

    add/ 로그인 login.java

--- PKNULibrary/app/src/main/java/com/example/pknulibrary/LoginActivity.java
+++ PKNULibrary/app/src/main/java/com/example/pknulibrary/LoginActivity.java
@@ -1,15 +1,39 @@
-
 package com.example.pknulibrary;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 
 public class LoginActivity extends Activity {
+
+    public static final int REQUEST_CODE_MENU = 101;
+
+    EditText etid, etpw;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_MENU) {
+            Toast.makeText(getApplicationContext(),
+                    "onActivityResult 메서드 호출됨. 요청코드 : " + requestCode + ", 결과코드 " + resultCode, Toast.LENGTH_LONG).show();
+
+            if (resultCode == RESULT_OK) {
+                String name = data.getStringExtra("name");
+                Toast.makeText(getApplicationContext(), "응답으로 전달된 name : " + name, Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +44,57 @@ public class LoginActivity extends Activity {
         Button button = (Button) findViewById(R.id.button); // sign in button
 
         // 버튼 실행
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(intent);
+        button.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+
+                etid = findViewById(R.id.editText3);
+                etpw = findViewById(R.id.editText4);
+
+                String inputid = etid.getText().toString();
+                String inputpw = etpw.getText().toString();
+
+                if (inputid.equals("201712059") && (inputpw.equals("201712059"))) {
+                    Toast.makeText(getApplicationContext(), "로그인성공", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE_MENU);
+                    finish();
+                    return true;
+
+                }
+                else if(inputid.equals("201712021") && (inputpw.equals("201712021"))){
+                    Toast.makeText(getApplicationContext(), "로그인성공", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE_MENU);
+                    finish();
+                    return true;
+                }
+                else if(inputid.equals("201410007") && (inputpw.equals("201410007"))){
+                    Toast.makeText(getApplicationContext(), "로그인성공", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE_MENU);
+                    finish();
+                    return true;
+                }
+                else if(inputid.equals("201512132") && (inputpw.equals("201512132"))){
+                    Toast.makeText(getApplicationContext(), "로그인성공", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE_MENU);
+                    finish();
+                    return true;
+                }else {
+                    Toast.makeText(getApplicationContext(), "로그인실패", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+
             }
+
+
+
         });
+
     }
-
 }
-
-
-
