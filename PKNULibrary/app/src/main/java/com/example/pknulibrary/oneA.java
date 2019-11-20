854a2a3fb5f21292e18f394a4984dcd9746b3a51
mj <>
2019-11-20 10:52:03 +0900

    add/ 1, 시간 표기법을 바꿨습니다. oneA.java 내에 구헌했는데, myPage에 외부적으로 종속되어있어서, 마이페이지 남은 시간에서 확인 하실 수 있습니다. 나머지 되시면 전부 이 코드로 복붙하셔도 될 것 같습니다. 2. 버튼 눌렀을 때 색깔 바뀌게는 했는데 빨강색으로는 못 만들었습니다(버튼 색 바꾸는 건 밝게, 어둡게, 회색조 스타일 밖에 없었습니다). 빨간색이나 노란색으로 만들려면 자바코드 말고 XML에서 해야할 것 같은데 그건 자바 파일 하나 더 만들어야 하고 컨텍스트 메세지도 띄워야 해서 일단 보류해뒀습니다. 하던거 일단 먼저 올립니다.

--- PKNULibrary/app/src/main/java/com/example/pknulibrary/oneA.java
+++ PKNULibrary/app/src/main/java/com/example/pknulibrary/oneA.java
@@ -20,6 +20,8 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import static android.graphics.BlendMode.COLOR;
+
 public class oneA extends AppCompatActivity {
 
     TextView display, text;
@@ -40,27 +42,24 @@ public class oneA extends AppCompatActivity {
         final String btn2text = btn2.getText().toString();
 
         btn1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
             public boolean onTouch(View arg0, MotionEvent arg1) {
-
-                // 마이페이지 설정
-                TimeZone timezone = TimeZone.getTimeZone("Etc/GMT-9");
-                TimeZone.setDefault(timezone);
-
-                SimpleDateFormat formater = new SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.KOREA);
-
-                Date current = new Date();
-
+// 시간 나타내는 코드 전부 수정했습니다~ 필요없어 보이는 부분 지우고 물결 뒤에 숫자 한 자리씩 나와서 두자리씩 포매팅 되게 만들었습니다.
                 String btn1text = btn1.getText().toString();
 
-                String mtime = formater.format(current);
-                System.out.println(mtime);
+                String thistime = null; //현재 시간 저장할 변수
+                String after6 = null;  //6시간 추가해서 저장할 함수
+                Date current = new Date();   //현재시간 불러오기
+                SimpleDateFormat formater = new SimpleDateFormat("yyyy.MM.dd HH:mm");  //포매팅
+                Calendar cal = Calendar.getInstance();  //cal 이라는 시간 인스턴스 만들어줌
+                cal.setTime(current);  //현재 시간 저장
 
-                TimeZone jst = TimeZone.getTimeZone ("JST");
-                Calendar cal = Calendar.getInstance(Locale.KOREA); // 주어진 시간대에 맞게 현재 시각으로 초기화된 GregorianCalender 객체를 반환.// 또는 Calendar now = Calendar.getInstance(Locale.KOREA);
-                System.out.println ( cal.get ( Calendar.YEAR ) + "." + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ "." +cal.get ( Calendar.MINUTE ));
-
-                display.setText(mtime+" ~ "+cal.get ( Calendar.YEAR ) + " . " + ( cal.get ( Calendar.MONTH ) + 1 ) + "." + cal.get ( Calendar.DATE ) + "." + (cal.get ( Calendar.HOUR_OF_DAY ) +6)+ ":" +cal.get ( Calendar.MINUTE ));
+                thistime = formater.format(cal.getTime()); //현재시간 문자열로 저장
+                cal.add(Calendar.HOUR, 6); //시간에 6시간 추가
+                after6 =formater.format(cal.getTime());; //추가한 걸 문자열로 저장
+                display.setText(thistime+ "~" + after6); //시간 붙여서 출력
                 text.setText(" "+btn1text+"번 좌석 사용 중");
+                //다른 버튼 눌렀을 때도 이 시간 포맷으로 수정해야 함
 
                 Intent intent=new Intent(oneA.this,MyPageActivity.class);
                 intent.putExtra("name",display.getText().toString());
@@ -68,10 +67,15 @@ public class oneA extends AppCompatActivity {
 
                 // 색 변경
 
-
+                if(arg1.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    arg0.setBackgroundColor(btn1.getResources().getColor(R.color.material_blue_grey_800));
+                }
 
                 return true;
             }
+
+
         });
 
 
